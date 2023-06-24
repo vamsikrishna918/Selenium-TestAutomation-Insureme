4c31c2f1a6989d04529ca13e88eda2c509f38f04
vamsikrishna918 <vamsiannadanam918@gmail.com>
2023-06-24 14:53:32 +0530

    push commit

---
+++ src/test/java/com/automationtest/SeleniumAutomationTest_Insureme/AppTest.java
@@ -0,0 +1,20 @@
+package com.automationtest.SeleniumAutomationTest_Insureme;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
